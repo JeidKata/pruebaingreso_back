87399ba925030b991a787056d5a7d11f51bc6034
JeidyOlaya <jeidkata@gmail.com>
2025-09-12 15:34:10 -0500

    chore: Configuro la ruta de la libreria GDAL. Recordar: GDAL(Geospatial Data Abstraction Library) es una biblioteca de software de código abierto esencial para trabajar con datos geoespaciales.

--- prueba_tecnica/settings.py
+++ prueba_tecnica/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
+os.environ['GDAL_LIBRARY_PATH'] = r'C:\Program Files\GDAL311\bin\gdal.dll'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
